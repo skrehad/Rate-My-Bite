ad87893ea35d5e789dfc4429dbc90c2d894e6941
JPChoyon <kotjpckhan@gmail.com>
2025-05-01 00:34:46 +0600

    primary secedary color and other color are set

---
+++ tailwind.config.js
@@ -0,0 +1,19 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./app/**/*.{js,ts,jsx,tsx}",
+    "./pages/**/*.{js,ts,jsx,tsx}",
+    "./components/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {
+      colors: {
+        primary: "#FF3C48",   // red
+        secondary: "#FF6168", // light red
+        tertiary: "#FF3C48",  // dark red
+        gray: "#F5F5F5", // light gray
+      },
+    },
+  },
+  plugins: [],
+};
