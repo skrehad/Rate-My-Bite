c08fe01bccaac374f725a03d4a602405cd35a7df
rehadhasan <rehadhasan664@gmail.com>
2025-05-02 23:58:42 +0600

    gallery section added

--- src/app/(withCommonLayout)/page.tsx
+++ src/app/(withCommonLayout)/page.tsx
@@ -1,12 +1,14 @@
 import FoodCategory from "@/components/home/category/FoodCategory";
+import Gallery from "@/components/home/Gallery/gallery";
 import Banner from "@/components/home/header/Banner";
 
 export default function Home() {
   return (
     <div>
       <Banner />
-      <FoodCategory/>
+      <FoodCategory />
 
+      <Gallery />
     </div>
   );
 }
---
+++ src/components/home/Gallery/gallery.tsx
@@ -0,0 +1,63 @@
+"use client";
+
+import { useEffect, useState } from "react";
+import Image from "next/image";
+
+const localImages = [
+  "https://i.postimg.cc/J0tJ4D8N/1.jpg",
+  "https://i.postimg.cc/xdwTbXLG/2.jpg",
+  "https://i.postimg.cc/52GfZXKc/3.jpg",
+  "https://i.postimg.cc/QtDZ31pz/4.jpg",
+  "https://i.postimg.cc/J4Fwh9NZ/5.jpg",
+  "https://i.postimg.cc/0yS4yr21/6.jpg",
+  "https://i.postimg.cc/J4D6ptXH/7.jpg",
+  "https://i.postimg.cc/QM0J8r5r/8.jpg",
+  "https://i.postimg.cc/G2fPDJq7/9.jpg",
+  "https://i.postimg.cc/23R4cjpn/10.jpg",
+];
+
+const GalleryPictureSlider = () => {
+  const [currentIndex, setCurrentIndex] = useState(0);
+  const [showingPictures, setShowingPictures] = useState<string[]>([]);
+
+  useEffect(() => {
+    const timer = setInterval(() => {
+      const nextIndex = (currentIndex + 1) % localImages.length;
+      const nextPictures = localImages.slice(nextIndex, nextIndex + 4);
+      setCurrentIndex(nextIndex);
+      setShowingPictures(nextPictures);
+    }, 3000);
+
+    return () => clearInterval(timer);
+  }, [currentIndex]);
+
+  return (
+    <div className="px-4 md:px-10">
+      <h1 className="text-center text-[32px] md:text-[40px] font-normal text-primary font-serif my-10">
+        Gallery
+      </h1>
+
+      <div className="grid gap-4 grid-cols-1 sm:grid-cols-2 lg:grid-cols-4">
+        {showingPictures.map((image, index) => (
+          <div
+            key={index}
+            className="relative w-full h-[240px] md:h-[280px] rounded overflow-hidden transition-all duration-1000 ease-in-out"
+          >
+            <Image
+              src={image}
+              alt={`Gallery image ${index + 1}`}
+              fill
+              className="object-cover rounded-lg"
+            />
+          </div>
+        ))}
+      </div>
+
+      <h2 className="text-center text-semibold  text-lg md:text-xl font-normal text-black font-serif my-10">
+        Only Admin Can Add Gallery Photo
+      </h2>
+    </div>
+  );
+};
+
+export default GalleryPictureSlider;
