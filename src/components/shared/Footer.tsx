c68d7b5ee4150440815e8b1c26e2a6b630228109
rehadhasan <rehadhasan664@gmail.com>
2025-05-03 21:47:08 +0600

    modify some design

--- src/components/shared/Footer.tsx
+++ src/components/shared/Footer.tsx
@@ -23,7 +23,7 @@ export default function Footer() {
             </p>
           </div>
 
-          {/* Navigation Links */}
+          {/* Navigation  */}
           <div>
             <h3 className="text-lg text-primary font-bold mb-2">Quick Links</h3>
             <ul className="space-y-2 text-[16px] font-medium ">
