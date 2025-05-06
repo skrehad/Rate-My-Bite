3e79ed2c4933c679fa7cc2a7dcdf16efbd5b095c
Al-amin07 <alsujon2001@gmail.com>
2025-05-06 19:30:52 +0600

    later

--- src/app/(withCommonLayout)/page.tsx
+++ src/app/(withCommonLayout)/page.tsx
@@ -1,7 +1,6 @@
 import FoodCategory from "@/components/home/category/FoodCategory";
 import Community from "@/components/home/community";
 import Discover from "@/components/home/discover";
-import FAQSection from "@/components/home/faq/FaqSection";
 import FeatureFoodSpot from "@/components/home/FeatureFoodSpot/FeatureFoodSpot";
 import { FoodEvents } from "@/components/home/food-event/FoodEvents";
 import Gallery from "@/components/home/Gallery/gallery";
