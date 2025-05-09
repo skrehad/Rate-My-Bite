0e8af2ee0653d51cc41380f3cab077e5cce970e5
Al-amin07 <alsujon2001@gmail.com>
2025-05-09 21:26:34 +0600

    dashboard changes

--- src/app/(withDashboardLayout)/user/posts/page.tsx
+++ src/app/(withDashboardLayout)/user/posts/page.tsx
@@ -2,6 +2,7 @@
 import { UserPostsTable } from '@/components/module/dashboard/user/UserPostTable';
 import { Button } from '@/components/ui/button';
 import { getAllPosts } from '@/services/dashboard/user';
+
 import Link from 'next/link';
 // import { getAllPosts } from '@/services/dashboard/admin'
 import React, { Suspense } from 'react'
@@ -12,12 +13,17 @@ export default async function UserPosts({ searchParams }: { searchParams: Promis
 
     return (
         <Suspense fallback={<div>Loading...</div>}>
-            <main className="container mx-auto py-10">
+            <main className="container mx-auto pt-3">
                 <div className='flex justify-between items-center mb-8'>
                     <h1 className="text-3xl font-bold ">Post Management</h1>
                     <Link href={'/createpost'}><Button>Create Post</Button></Link>
                 </div>
-                <UserPostsTable data={data?.data} meta={data?.meta} />
+                {
+                    data?.data && data?.data?.length === 0 ? <UserPostsTable data={data?.data} meta={data?.meta} /> : <div className='flex justify-center items-center bg-gray-50 rounded-md h-40'>
+
+                        <h1 className='text-2xl font-semibold text-slate-700'> No Post Found</h1>
+                    </div>
+                }
             </main>
         </Suspense>
     )
